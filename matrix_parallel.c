2a1b8cb7d4b47e1e33bb34e42a7b2827e957063f
cuike9@gmail <cuike9@gmail>
2018-06-22 22:09:14 +0900

    bug

--- matrix_parallel.c
+++ matrix_parallel.c
@@ -34,38 +34,65 @@ int main(int argc, char* argv[])
         for (j = 0; j < NCB; j++)
         {
             for (i = 0; i < NCA; i++)
-                matrix_b[j][i] = i * j;
+                matrix_b[j][i] = i + j;
 
         }
-        for (i = 0; i < NRA; i++)
-            for (j = 0; j < NRA; j++)
-                printf("%f\n", matrix_a[i][j]);
-
-        printf("\n\n");
         for (i = 1; i < size; i++)  
-        {    MPI_Send(matrix_a + i, NRA, MPI_DOUBLE, i, tag, MPI_COMM_WORLD);
-             MPI_Send(matrix_b + i, NCA, MPI_DOUBLE, i, tag, MPI_COMM_WORLD);
+        {
+             MPI_Send(matrix_a + i, NRA, MPI_DOUBLE, i, tag, MPI_COMM_WORLD);
+             MPI_Send(matrix_b + i, NCB, MPI_DOUBLE, i, tag, MPI_COMM_WORLD);
         }
+        MPI_Barrier(MPI_COMM_WORLD);
+        int index = my_rank;
+        do{
+            matrix_c[my_rank][index] = 0;
+            for (i = 0; i < NCB; i++)
+            {
+                matrix_c[my_rank][index] += matrix_a[my_rank][i] * matrix_b[my_rank][i];
+            }
+            MPI_Send(vector_b, NCB, MPI_DOUBLE, my_rank + 1, tag, MPI_COMM_WORLD);
+            MPI_Recv(vector_b, NCB, MPI_DOUBLE, size - 1, tag, MPI_COMM_WORLD, &status);
+            index++; 
+            MPI_Barrier(MPI_COMM_WORLD);  
+        }while(index != size);
+
+        printf("breakloop? processor 0 %d", my_rank);
     }
     
             
     if (my_rank != 0) 
     {
-        double vector_a[NRA], vector_b[NRA], vector_c[NRA];
+        double vector_a[NCA], vector_b[NCB], vector_c[NCB];
         MPI_Recv(vector_a, NRA, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD, &status);
-        MPI_Recv(vector_b, NCA, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD, &status);
-        for (int i = 0; i < NRA; i++) 
+        MPI_Recv(vector_b, NCB, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD, &status);
+        MPI_Barrier(MPI_COMM_WORLD);
+//        for (int i = 0; i < NRA; i++) 
+//        {
+//            printf("processor:%d a  %f      \n", my_rank,vector_a[i]);
+//        }
+//        for (int i = 0; i < NCA; i++)
+//        {
+//            printf("processor:%d b  %f      \n", my_rank, vector_b[i]);
+//        }
+        
+        int index = my_rank;
+        do {
+ //          vector_c[index] = 0;
+            for (int i = 0; i < NCB; i++)
+            {
+                vector_c[index] += vector_a[i] * vector_b[i]; 
+            }
+            MPI_Send(vector_b, NCB, MPI_DOUBLE, (my_rank + 1) % size, tag, MPI_COMM_WORLD);
+            MPI_Recv(vector_b, NCB, MPI_DOUBLE, my_rank - 1, tag, MPI_COMM_WORLD, &status);
+            MPI_Barrier(MPI_COMM_WORLD);
+            index = (index + 1) % size;    
+        }while(index != my_rank - 1); 
+        for (int i = 0; i < NCB; i++)
         {
-            printf("processor:%d  %f      \n", my_rank,vector_a[i]);
-        }
-        for (int i = 0; i < NCA; i++)
-        {
-            printf("processor:%d  %f      \n", my_rank, vector_b[i]);
-        }
-        
-        
-        
+            printf("vector_c processor:%d    %f\n", my_rank, vector_c[i]);
+        }        
     }
+
 
     MPI_Finalize();
 }
